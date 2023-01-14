2fd621a89af0496c466d2cd480f5b52f82885d8c
Timur Garipov <tr.garipov@yandex.ru>
2023-01-14 20:46:26 +0300

    connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String DB_USERNAME = "postgres";
+    private final static String DB_PASSWORD = "0000";
+    private final static String DB_URL = "jdbc:postgresql://127.0.0.1:5432/taskDB";
+
+
+    public static void connectionToBD() {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
